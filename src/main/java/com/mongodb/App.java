b2ac5e41cfd7706cad3bc4bf4df2f5f0a8a84b8c
aashomok <aashomok@unisender.com>
2018-01-23 08:58:39 +0200

    add mongo client

--- src/main/java/com/mongodb/App.java
+++ src/main/java/com/mongodb/App.java
@@ -1,12 +1,14 @@
 package com.mongodb;
 
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
+import org.bson.Document;
 import spark.Spark;
 
 import java.io.StringWriter;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Objects;
 
 /**
  * Hello world!
@@ -18,20 +20,37 @@ public class App
         Configuration configuration = new Configuration();
         configuration.setClassForTemplateLoading(App.class, "/");
 
+        MongoClient mongoClient = new MongoClient();
+        MongoDatabase database = mongoClient.getDatabase("course");
+        MongoCollection<Document> collection = database.getCollection("hello");
+        collection.drop();
+
+        collection.insertOne(new Document("name", "MongoDB"));
+
         Spark.get("/", (request, response) -> {
             StringWriter writer = new StringWriter();
             try {
                 Template template = configuration.getTemplate("hello.ftl");
 
-                Map<String, Object> map = new HashMap<String, Object>();
-                map.put("name", "Andrii");
+                /*Map<String, Object> map = new HashMap<String, Object>();
+                map.put("fruits", Arrays.asList("apple", "orange", "banana", "peach"));*/
 
-                template.process(map, writer);
+                Document document = collection.find().first();
+                template.process(document, writer);
+
             } catch (Exception e) {
                 writer.append(e.getMessage());
             }
 
             return writer;
         });
+
+        Spark.post("/fruit", (request, response) -> {
+            String fruit = request.queryParams("fruit");
+            if (Objects.nonNull(fruit)) return "Your fruit is ".concat(fruit); else return "Choose fruit";
+        });
+
+
+        Spark.get("/echo/:thing", (request, response) -> request.params(":thing"));
     }
 }
